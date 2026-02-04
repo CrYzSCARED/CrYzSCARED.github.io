d089b05bcc25bfc0e48517424a5ccdc9f69d599f
A.Alessio <alessio.adriani84@gmail.com>
2026-02-04 13:51:41 +0100

    Add ScrollToTop component for automatic scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";   // ✅ NEU
+
 import Index from "./pages/Index";
 import Leistungen from "./pages/Leistungen";
 import UeberMich from "./pages/UeberMich";
@@ -16,7 +19,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <HashRouter>
+        {/* ✅ Scrollt bei Seitenwechsel automatisch nach oben */}
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/leistungen" element={<Leistungen />} />
@@ -25,8 +32,10 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
+
